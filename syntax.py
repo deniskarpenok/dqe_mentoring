8b89b26f71b96a789438be1a75a56fe495418175
Denis Karpenok <dinas@MacBook-Pro-Denis.local>
2019-11-10 17:24:10 +0300

    Types

---
+++ syntax.py
@@ -0,0 +1,48 @@
+
+##  None type
+a = None
+print(type(a))
+
+# # boolean
+# b = True
+# print(type(b))
+#
+# # int
+# c = 1
+# print(type(c))
+#
+#
+# # float
+# d = 0.1
+# print(type(d))
+# print(d * 10)
+# print(d + d + d + d + d + d + d + d + d + d)
+#
+# # list
+# e = [1, 2, 3]
+# print(type(e))
+# print(e.__sizeof__())
+#
+# #corteg
+# f = (1, 2, 3)
+# print(type(f))
+# print(f.__sizeof__())
+#
+# #string
+# g = 'hello, World'
+# print(type(g))
+#
+# # dictionary
+# h = {'first_key': 1, 'second_key': 2}
+# print(type(h))
+#
+# i = {1, 2, 3}
+# print(type(i))
+# i = set(g)
+# print(i)
+#
+#
+# # etc, e.g. complex
+# x = complex(1, 2)
+# print(x)
+# print(type(x))
